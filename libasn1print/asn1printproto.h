dcc71ba78e74678d6419f67cbf359dca5ed01a23
Sean Condon <sean@opennetworking.org>
2020-07-19 17:41:43 +0100

    Compile protobuf enums from ASN enums (#2)
    * Compile protobuf enums from ASN enums
    
    Also moved proto procesing out in to its own file
    
    Added a unit test for proto enums
    
    * Added extra output to travis on failure
    
    * Just print the filename in protobuf (not the whole path)
    
    * Proto function - minor conversion

---
+++ libasn1print/asn1printproto.h
@@ -0,0 +1,31 @@
+// Copyright 2020-present Open Networking Foundation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef LIBASN1PRINT_ASN1PRINTPROTO_H_
+#define LIBASN1PRINT_ASN1PRINTPROTO_H_
+
+enum asn1print_flags2 {
+	APF_NOFLAGS2,
+	APF_NOINDENT2		= 0x01,	/* Disable indentation */
+	APF_LINE_COMMENTS2	= 0x02, /* Include line comments */
+	APF_PRINT_XML_DTD2	= 0x04,	/* Generate XML DTD */
+	APF_PRINT_CONSTRAINTS2	= 0x08,	/* Explain constraints */
+	APF_PRINT_CLASS_MATRIX2	= 0x10,	/* Dump class matrix */
+	APF_PRINT_PROTOBUF2	= 0x20,	/* Generate Protobuf */
+};
+
+int asn1print_expr_proto(asn1p_t *asn, asn1p_module_t *mod, asn1p_expr_t *tc, enum asn1print_flags2 flags, int level);
+int toLowercase(char *mixedCase, char **lowerCase);
+
+#endif /* LIBASN1PRINT_ASN1PRINTPROTO_H_ */
